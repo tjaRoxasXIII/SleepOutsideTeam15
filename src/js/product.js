894141793fc1047b2056c5b2c4d78709020d8896
Neron56 <Michaelshaw20004@gmail.com>
2026-07-02 19:58:19 -0600

    week 1 team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage,setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const currentCart = getLocalStorage('so-cart') || [];
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  currentCart.push(product);
+  setLocalStorage("so-cart", currentCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
