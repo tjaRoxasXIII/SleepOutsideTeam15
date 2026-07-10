57b4adb49b6b4b4eee7a046d272716aa5cb37a25
tjaRoxasXIII <tjaRoguetech@gmail.com>
2026-07-10 09:50:12 -0400

    fixing one instance of single-quote mis-use

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,7 @@ import { getLocalStorage,setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const currentCart = getLocalStorage('so-cart') || [];
+const currentCart = getLocalStorage("so-cart") || [];
 
 function addProductToCart(product) {
   currentCart.push(product);
