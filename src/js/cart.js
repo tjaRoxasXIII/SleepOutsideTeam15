390a41b663c6d0f72c9b222042b6fc950b79bf79
tjaRoxasXIII <tjaRoguetech@gmail.com>
2026-07-10 19:28:36 -0400

    Adding in the cart removal elements and styling.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,50 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const htmlItems = cartItems.map((item, index) =>
+    cartItemTemplate(item, index),
+  );
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  document.querySelectorAll(".cart-removal").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const index = parseInt(e.currentTarget.dataset.index);
+      removeCartItem(index);
+    });
+  });
 }
 
-function cartItemTemplate(item) {
+function cartItemTemplate(item, index) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    
+  </li>
+  <span span class="cart-removal" data - index="${index}" > X</span >`
+  ;
 
   return newItem;
+}
+
+function removeCartItem(index) {
+  const cart = getLocalStorage("so-cart") || [];
+
+  cart.splice(index, 1);
+
+  setLocalStorage("so-cart", cart);
+
+  renderCartContents();
 }
 
 renderCartContents();
